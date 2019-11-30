2e030524ee6e48c422f3233cdef2afc6cf651be3
usama <usama.bsse3222@iiu.edu.pk>
2019-11-30 23:04:02 +0500

    part 3 completed

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ app.listen(3000, () => {
   console.log('Express application running on localhost:3000');
 });*/
 //mongoose.connect('mongodb://localhost:27017/myfirstmongodb', {useNewUrlParser: true});
-
+const jsonwebtoken = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const express = require('express');
 const app = express();
 var mongoose = require('mongoose');
@@ -63,7 +64,12 @@ app.get('/', (req, res) => {
     res.send(listOfStudents);
   });*/
  
-
+  const Users = mongoose.model('Users', {
+    name: String,
+    email: String,
+    password: String,
+    date_added: Date
+   });
 
 
   const Student = mongoose.model('Student', {
@@ -91,7 +97,7 @@ app.get('/', (req, res) => {
     
       });*/
 
- app.post('/signup', async (req, res) => {
+ /*app.post('/signup', async (req, res) => {
 const body = req.body;
 console.log('req.body', body);
   try{
@@ -107,7 +113,100 @@ res.send({
     console.log('ex',ex);
     res.send({message: 'Error'}).status(401);
   }
-});
+});*/
+
+/*app.post('/signup', async (req, res) => {
+
+  try{
+    const body = req.body;
+
+    // there must be a password in body
+
+    // we follow these 2 steps
+
+    const password = body.password;
+
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+
+  
+  
+    body.password = hash;
+    
+    console.log('hash - > ', hash);
+    const user = new Users(body);
+
+
+     const result = await user.save();
+
+     res.send({
+     message: 'Student signup successful'
+    });
+
+  }
+
+  catch(ex){
+    console.log('ex',ex)
+
+    res.send({
+      message: 'Error',
+      detail: ex
+    }).status(500);
+  }
+
+  });*/
+  app.post('/login', async (req, res) => {
+    try {
+      const body = req.body;
+  
+      const email = body.email;
+  
+      // lets check if email exists
+  
+      const result = await Users.findOne({ email: email });
+      if (!result) {
+        // this means result is null
+        res.status(401).send({
+          Error: 'This user doesnot exists. Please signup first'
+        });
+      } else {
+        
+        // email did exist
+        // so lets match password
+  
+        //if (body.password === result.password)
+        if ( bcrypt.compareSync(body.password, result.password)) {
+          // great, allow this user access
+  
+          // great, allow this user access
+
+        console.log('match');
+
+        result.password = undefined;
+        //delete result['password'];
+
+        const token = jsonwebtoken.sign({
+           data: result,
+           role: 'User'
+        }, 'supersecretToken', { expiresIn: '7d' });
+
+        console.log('token -> ', token)
+
+        res.send({ message: 'Successfully Logged in', token: token });
+        } else {
+          console.log('password doesnot match');
+  
+          res.status(401).send({ message: 'Wrong email or Password' });
+        }
+      }
+    } catch (ex) {
+      console.log('ex', ex);
+    }
+  });
+
+
+
+
 
 
 
